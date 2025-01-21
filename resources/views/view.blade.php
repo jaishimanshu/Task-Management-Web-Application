cac948ac1722c7a19175191cb73251a161db13f5
Himanshu Jaiswal <104904143+jaishimanshu@users.noreply.github.com>
2025-01-21 22:24:00 +0530

    Add files via upload

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -1,12 +1,17 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>  Laravel CKEditor Example </title>
+        <title> User Task Updadte</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
         <style>
             table td p {
                 word-break: break-all;
@@ -17,20 +22,38 @@
         <div class="container mt-4">
             <div class="row">
                 <div class="col-xl-8">
-                    <h3 class="text-right"> Laravel CKEditor Example</h3>
+                    <h3 class="text-right"><b>User Task Updadte</b></h3>
                 </div>
-                <div class="col-xl-4 text-right">
-                    <a href="{{url('/')}}" class="btn btn-primary"> Back</a>
-                </div>
-                <input type="radio" id="ALL" name="data_sel">
-                <label for="ALL">ALL</label><br>
-                <input type="radio" id="Completed" name="data_sel">
-                <label for="Completed">Completed</label><br>
-                <input type="radio" id="not_complted" name="data_sel">
-                <label for="not_complted">Not Completed</label>
+               
             </div>
+                <div class="row">
+                    <div class="col-md-8 form-group">
+                        <form action="{{ url('view') }}" method="GET">
+                            <div class="col-md-2">
+                                <input type="radio" id="ALL" name="data_sel" value="ALL" 
+                                       @if(request('data_sel') == 'ALL') checked @endif>
+                                <label for="ALL">ALL</label><br>
+                            </div>
+                            <div class="col-md-2">
+                                <input type="radio" id="Completed" name="data_sel" value="Completed"
+                                       @if(request('data_sel') == 'Completed') checked @endif>
+                                <label for="Completed">Completed</label><br>
+                            </div>
+                            <div class="col-md-3">
+                                <input type="radio" id="not_completed" name="data_sel" value="not_completed"
+                                       @if(request('data_sel') == 'not_completed') checked @endif>
+                                <label for="not_completed">Not Completed</label><br>
+                            </div>
+                            <button type="submit">Submit</button>
+                        </form>
+                    </div>
+                        <div class="col-xl-4 text-right">
+                            <a href="{{url('/')}}" class="btn btn-primary"> Back</a>
+                        </div>
+                </div>
+
             <div class="table-responsive mt-4">
-                <table class="table table-striped table-bordered">
+                <table class="table table-striped table-bordered" id="myTable">
                     <thead>
                         <tr>
                             <th> Id </th>
@@ -59,10 +82,10 @@
                                     onclick="updateTaskStatus({{ $task->id }})" {{ $task->status ? 'checked' : '' }}>
                                 </td>
 
-                                <td ><a href="{{ url('/edit/' . $task->id) }}" title="Click here to edit the task">
-                                    <i class="fa fa-edit"></i>
+                                <td ><a href="{{ url('/edit/' . $task->id) }}" title="Click here to edit the task" style="cursor: pointer;font-size: 22px;color: green;">
+                                    <i class="fa fa-edit"></i></a>
 
-                                <a onclick="deleteTaskStatus({{ $task->id }})"  title="Click here to delete the task"><i class="fa fa-trash"></i></a> </td>
+                                <a onclick="deleteTaskStatus({{ $task->id }})"  title="Click here to delete the task" style="cursor: pointer;float: right;font-size: 22px;color: red;"><i class="fa fa-trash"></i></a> </td>
                             </tr>
                         @endforeach
                     </tbody>
@@ -70,6 +93,13 @@
             </div>
             {{ $tasks->links() }}
         </div>
+        <script type="text/javascript">
+    var $jq = jQuery.noConflict(); 
+
+            $jq(document).ready(function() {
+                $('#myTable').DataTable();
+            });
+            </script>
 <script>
     var $jq = jQuery.noConflict(); 
     function updateTaskStatus(value) {
@@ -88,30 +118,6 @@
                 } else {
                     console.log('Error updating status');
                 }
-            },
-            error: function() {
-                console.log('An error occurred');
-            }
-        });
-    }
-
-
-    $jq('input[name="data_sel"]').on('change', function() {
-        var selectedFilter = $jq('input[name="data_sel"]:checked').val();
-        filterTasks(selectedFilter); 
-    });
-
-    function filterTasks(filter) {
-        $jq.ajax({
-            url: '/filter-tasks', 
-            method: 'GET',
-            data: {
-                _token: '{{ csrf_token() }}',
-                filter: filter,
-            },
-            success: function(response) {
-                $jq('.table tbody').html(response.tasks_html);
-                $jq('.pagination').html(response.pagination);
             },
             error: function() {
                 console.log('An error occurred');
